9b15e9d66e7647fb82eed6ac2a01e1907eb63973
zaradai <stephen_c_hough@hotmail.com>
2014-04-26 16:36:53 +0800

    Add connection authentication

---
+++ java/common/src/main/java/com/zaradai/net/authentication/AcceptIfOnApprovedListConnectionAuthenticator.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2014 Zaradai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zaradai.net.authentication;
+
+import com.google.common.collect.Sets;
+
+import java.net.InetSocketAddress;
+import java.util.Set;
+
+public class AcceptIfOnApprovedListConnectionAuthenticator implements ConnectionAuthenticator {
+    private final Set<InetSocketAddress> approved;
+
+    public AcceptIfOnApprovedListConnectionAuthenticator() {
+        approved = createApprovedList();
+    }
+
+    protected Set<InetSocketAddress> createApprovedList() {
+        return Sets.newHashSet();
+    }
+
+    @Override
+    public boolean authenticate(InetSocketAddress connector) {
+        return approved.contains(connector);
+    }
+
+    public void add(InetSocketAddress address) {
+        approved.add(address);
+    }
+
+    public void remove(InetSocketAddress address) {
+        approved.remove(address);
+    }
+}
